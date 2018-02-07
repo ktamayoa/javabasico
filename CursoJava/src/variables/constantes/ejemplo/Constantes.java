819092bf8dbd95cc277510b43e6e010aa15286ed
Chris Inapanta <crisantip@hotmail.com>
2018-02-06 21:15:34 -0500

    Agrega ejemplos de Variables y Constantes

---
+++ CursoJava/src/variables/constantes/ejemplo/Constantes.java
@@ -0,0 +1,23 @@
+package variables.constantes.ejemplo;
+
+public class Constantes {
+
+	// Constante de clase
+	final static double PI = 3.14;
+	
+	// Constante de instancia (global)
+	final double iva = 12;
+	
+	public static void main(String[] args) {
+		
+		// Constante de metodo (local)
+		final String NEMONICO = "CC";
+		
+		System.out.println(NEMONICO);
+		
+		// TODO Explicacion a futuro
+		System.out.println(Constantes.PI);
+		Constantes instanciaConstantes = new Constantes();
+		System.out.println(instanciaConstantes.iva);
+	}
+}
---
+++ CursoJava/src/variables/constantes/ejemplo/Variables.java
@@ -0,0 +1,35 @@
+package variables.constantes.ejemplo;
+
+public class Variables {
+
+	public static void main(String[] args) {
+
+		int _altura = 165;
+		System.out.println(_altura);
+		boolean esFinalizado = false;
+		System.out.println(esFinalizado);
+		float $promedio;
+		$promedio = 3.2F;
+		System.out.println($promedio);
+		int c = 4 * 3;
+		System.out.println(c);
+		char clave = 'C';
+		System.out.println(clave);
+		String nombre = "Katty";
+		System.out.println(nombre);
+		String apellido = "Tamayo";
+		System.out.println(apellido);
+		String otraVariable = nombre = apellido = "prueba";
+		System.out.println(otraVariable);
+
+		// Ejemplos que no cumplen con las reglas de nombrado
+		// int 4share;
+		// int _var 1;
+		// int $var.2;
+		
+		// Ejemplos que no cumplen con las convenciones de nombrado
+		// int asdlkasdjas;
+		// int Hola;
+		// float mivaRIaBLELocaCC;
+	}
+}
