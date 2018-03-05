868a5f685f8d69103f217d927d4c6a3208716673
Katty Tamayo <kata_alexa@hotmail.com>
2018-03-04 23:51:38 -0500

    Uso de paquetes

---
+++ CursoJava/src/paquetes/ejemplo/_1ConstantesCortas.java
@@ -0,0 +1,12 @@
+package paquetes.ejemplo;
+
+//acceder a las constantes de la clase Math
+import static java.lang.Math.*;
+
+public class _1ConstantesCortas {
+
+	public static void main(String[] args) {
+		System.out.println("PI: " + PI);
+		System.out.println("PI cambiado: " + (PI*3));
+	}
+}
