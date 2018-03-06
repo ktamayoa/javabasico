e9fe299865fadef99d107c5a5d4c9503cc196606
Katty Tamayo <kata_alexa@hotmail.com>
2018-03-05 21:08:28 -0500

    Nuevo ejemplo paquetes

---
+++ CursoJava/src/paquetes/ejemplo/_2ImportClasesIguales.java
@@ -0,0 +1,14 @@
+package paquetes.ejemplo;
+import java.sql.*;
+import java.util.*;
+
+public class _2ImportClasesIguales {
+
+	
+	public static void main(String[] args) {
+		//Date now = new Date();  //incorrecto
+		java.util.Date fecha = new java.util.Date(); //correcto 
+
+	}
+
+}
