d7cc285cae6e62e0e75148853c075048a642aaea
Chris Inapanta <crisantip@hotmail.com>
2018-05-31 21:41:12 -0500

    Agrega primer ejemplo de OCA

---
+++ OCA/src/chapter1/Classes.java
@@ -0,0 +1,17 @@
+package chapter1;
+
+public class Classes {	// Solo una puede ser public y el nombre del archivo debe ser del mismo nombre
+
+}
+
+class Classes2 { // puede tener muchas clases sin modificador de acceso
+
+}
+
+class Classes3 {
+
+}
+
+//public class Classes4 {   // Esto no es permitido ya que existe una public, debería ponerse esta clase en su propio archivo.
+//
+//}
