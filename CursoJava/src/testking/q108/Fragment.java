c0c16ec66486f964d46f2794d69188d82072d713
csi <crisantip@hotmail.com>
2019-05-20 23:29:42 -0500

    Agrega ejercicio 108

---
+++ CursoJava/src/testking/q108/Fragment.java
@@ -0,0 +1,37 @@
+package q108;
+
+public class Fragment {
+
+	public static void main(String[] args) {
+		int[] stack = { 10, 20, 30 };
+		int size = 3;
+		int idx = 0;
+
+		// Opción A
+//		do {
+//			idx++;
+//		} while (idx >= size);
+
+		// Opción B
+//		while (idx < size) {
+//			idx++;
+//		}
+
+		// Opción C
+		do {
+			idx++;
+		} while (idx < size - 1);
+
+//		// Opción D
+//		do {
+//			idx++;
+//		} while (idx <= size);
+//
+//		// Opción E
+//		while (idx <= size - 1) {
+//			idx++;
+//		}
+		
+		System.out.println("The top element: " + stack[idx]);
+	}
+}
