1f475d76609b785c56ca9ab6667fba7b915f8d21
Katty Tamayo <kata_alexa@hotmail.com>
2019-05-20 19:03:58 -0500

    Pregunta 95

---
+++ CursoJava/src/testking/q95/_95C.java
@@ -0,0 +1,34 @@
+package testking.q95;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class A {
+	public void test(){
+		System.out.println("A");
+	}
+}
+
+class B extends A{
+	public void test(){
+		System.out.println("B");
+	}
+}
+
+
+public class _95C extends A{
+
+	public void test(){
+		System.out.println("C");
+	}
+	public static void main(String[] args) {
+		A b1 = new A();
+		A b2 = new _95C();
+		
+		b1 = (A)b2;
+		A b3 = (B)b2; //testking.q95._95C cannot be cast to testking.q95.B
+		
+		b1.test();
+		b3.test();
+	}
+}
