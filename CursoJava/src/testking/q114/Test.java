bade0b61db45cb4571d40b907084b23070c2d837
Christian Inapanta <crisantip@gmail.com>
2019-05-21 13:34:28 -0500

    Agrega explicación

--- CursoJava/src/testking/q114/Test.java
+++ CursoJava/src/testking/q114/Test.java
@@ -1,22 +1,27 @@
 package q114;
 
+/*
+ * Si se ejecuta la clase Test con los siguientes comandos, cuál es el resultado
+ * javac Test.java
+ * java Test
+ */
 
 public class Test {
 	public static final int MIN = 1;
 	
 	public static void main(String[] args) {
 	
-		int x = args.length;
+		int x = args.length; // x = 0
 		
 		if (checkLimit(x)){ //line n1
 			System.out.println("Java SE");
 		} else{
-			System.out.println("Java EE");
+			System.out.println("Java EE"); // Despliega "Java EE" (Respuesta: B)
 		}
 	}
 
 	private static boolean checkLimit(int x) {
-		return (x >= MIN) ? true : false;
+		return (x >= MIN) ? true : false; // 0 >= 1 (false) .: retorna false
 	}
 	
 }
