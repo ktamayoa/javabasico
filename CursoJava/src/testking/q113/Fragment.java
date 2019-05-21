1cde8f6d45c6b2420638364d321387fba1d4f385
Christian Inapanta <crisantip@gmail.com>
2019-05-21 13:30:33 -0500

    Agrega comentarios

--- CursoJava/src/testking/q113/Fragment.java
+++ CursoJava/src/testking/q113/Fragment.java
@@ -8,9 +8,10 @@ public class Fragment {
 	public static void main(String[] args) {
 
 		LocalDateTime dt = LocalDateTime.of(2014, 7, 31, 1, 1);
-		dt.plusDays(30);
-		dt.plusMonths(1);
-		System.out.println(dt.format(DateTimeFormatter.ISO_DATE));
+		// Los objetos LocalDateTime son inmutables
+		dt.plusDays(30); // Agrega 30 días a la fecha dt y devuelve una copia de la nueva fecha
+		dt.plusMonths(1); // Agrega 1 mes a la fecha dt y devuelve una copia de la nueva fecha
+		System.out.println(dt.format(DateTimeFormatter.ISO_DATE)); // Imprime la fecha original en formato yyyy-MM-dd -> 2014-07-31 (Respuesta: C)
 		
 	}
 	
